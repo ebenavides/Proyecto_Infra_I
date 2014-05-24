2b2c396e1cd611d95d445aeddbe5b463b9469d23
Esteban Benavides <ebenavidescruz@gmail.com>
2014-05-23 18:53:22 -0600

    Codigo del Proyecto. Ver01.1
    Proyecto, Error de ensamblado corregido.

---
+++ Proyecto_Infra/WindowsFormsApplication2/Config.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    public class Config
+    {
+        //Sincronizacion
+        public static string send;
+        public static string receive;
+
+        //Direccionamiento
+        public static string dir_explicit;
+        public static string dir_implicit;
+        public static string dir_static;
+        public static string dynamic;
+
+        //Formato
+        public static string content;
+        public static string fmt_length;
+
+        //Cola
+        public static string length;
+        public static bool fifo;
+        public static bool priority;
+    }
+}
---
+++ Proyecto_Infra/WindowsFormsApplication2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication2
+{
+      class Program
+    {
+
+        public static List<Object> Procesos = new List<Object>();
+        public static Config config = new Config();
+        
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            new System.Threading.Thread(() => { System.Windows.Forms.Application.Run(new ConfigWindow(config)); }).Start();
+            while (true)
+            {
+                string command = Console.ReadLine();
+                    new System.Threading.Thread(() => {System.Windows.Forms.Application.Run(new ProcesoWindow());}).Start();
+               }
+            }
+        }
+    }
+
