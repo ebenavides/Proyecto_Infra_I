d8ae2efd5a525f054fa4b0d97e3aa2dcb44ef5c3
Esteban Benavides <ebenavidescruz@gmail.com>
2014-05-26 01:58:37 -0600

    Prueba

--- Proyecto_Infra/Proyecto_Infra/Program.cs
+++ Proyecto_Infra/Proyecto_Infra/Program.cs
@@ -16,11 +16,14 @@ namespace Proyecto_Infra
         public static ArrayList cola = new ArrayList();
         public static string line = "";
         public static String[] command_args;
+        
+
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            
             ConfigWindow configw = new ConfigWindow();
             configw.ShowDialog();
             Console.WriteLine("Configuracion Inicial:\nSend: " + Config.send + "\nReceive: " + Config.receive + "\n\nDireccionamiento: " + Config.direccionamiento + "\n\nMensaje\nTipo: " + Config.content + "\nLargo: " + Config.fmt_length + "\n\nCola\nFIFO: " + Config.fifo.ToString() + "\nPrioridad: " + Config.priority.ToString() + "\nLargo: " + Config.length.ToString());
@@ -39,11 +42,20 @@ namespace Proyecto_Infra
 
         public static void Reader(string command){
 
+
+
             //verifica si es comando send
             if(command.Contains("send")){
                 command = command.Replace("send(", "");
                 command = command.Replace(")", "");
                 command_args = command.Split(',');
+                foreach(string arg in command_args){
+                    Console.WriteLine(arg);
+                }
+                /*int idemisor = int.Parse(command_args[0]);
+                int idreceptor = int.Parse(command_args[1]);
+                string mensaje = command_args[2];*/
+
 
                 
             }
